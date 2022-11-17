1763ccc66320b47438fbb8d82db16aa72cdc9d51
Shreya <shreyagupta4563@gmail.com>
2022-11-17 22:41:55 +0530

    added file

---
+++ p1.js
@@ -0,0 +1,19 @@
+
+console.log("Hello world");
+// console.warn("This is warning!");
+// variables - container to store data values
+/*
+multi
+line
+comment
+ */
+// Data types
+var num1 = 23;
+var num2 = 34;
+console.log(num1 + num2);
+// console.log(NaN == NaN ? "yes" : "No");
+// document.getElementById('#b1').style.border(solid);
+const button1 = document.querySelector('.b1');
+button1.addEventListener('click', function () {
+    document.querySelector('h1').style.color = 'blue';
+});
