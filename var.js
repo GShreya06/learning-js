317714bc080710c520d53226fcbaf93bac7a842d
Shreya <shreyagupta4563@gmail.com>
2023-01-23 22:26:20 +0530

    primitive data types added

--- var.js
+++ var.js
@@ -1,3 +1,16 @@
 var a=8;
 var  a=hello;
 console.log(a)
+
+// Primitive data types 
+// nn bb ss u
+
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("353")+BigInt("56");
+let e = "Hello";
+let f = Symbol("I am a Symbol");
+let g
+console.log(a,b,c,d,e,f,g)
+console.log(typeof g)
